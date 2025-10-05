90c49c3e256b334e58e7014fabfb764690ebce8f
JayTongue <142804110+JayTongue@users.noreply.github.com>
2025-10-04 20:54:23 -0500

    sketched out functions for stats.c

--- ese-coursera-course1/assessments/m1/stats.c
+++ ese-coursera-course1/assessments/m1/stats.c
@@ -9,23 +9,59 @@
  *
  *****************************************************************************/
 /**
- * @file <Add File Name> 
- * @brief <Add Brief Description Here >
+ * @file stats.c
+ * @brief Assignment submission for Module 1
  *
- * <Add Extended Description Here>
+ * Implements seven functions besides main():
+ * print_statistics(): prints stats of an array including min, man, mean, and med
+ * print_array(): given array of data and length, prints the array to screen
+ * find_median(): given array of data and length, returns the median value
+ * find_mean(): given array of data and length, returns the mean value
+ * find_maximum(): given array of data and length, returns the max val
+ * find_minimum(): given array of data and length, returns the min val
+ * sort_array(): given array of data and lendth, sorts array in reverse value.
  *
- * @author <Add FirsName LastName>
- * @date <Add date >
+ * @author Justin Tung
+ * @date 2025-10-04
  *
  */
-
 
 
 #include <stdio.h>
 #include "stats.h"
 
-/* Size of the Data Set */
-#define SIZE (40)
+#define SIZE 40 //size of array
+
+void print_array(unsigned char *test, int size) {
+ // loop through array items and printf each one
+}
+
+int find_mean(unsigned char *test, int size) {
+  // find sum of all items and divivde by array length
+}
+
+int find_max(unsigned char *test, int size) {
+  // return first item in reverse-sorted array
+}
+
+int find_min(unsigned char *test, int size, int max) {
+  // return last item in reverse-sorted array
+}
+
+void sort_array(unsigned char *test, int size) {
+  // Implement sorting algo
+  // Call this function before calculating other parameters
+}
+
+int find_median(unsigned char *test, int size) {
+  // if array has even items, then average the two center items
+  // if array has odd items, return middle item.
+}
+
+void print_statistics(int max, int min, int median, int mean) {
+  // print each parameter
+}
+
 
 void main() {
 
@@ -36,7 +72,22 @@ void main() {
                                 7,  87, 250, 230,  99,   3, 100,  90};
 
   /* Other Variable Declarations Go Here */
+  int mean;
+  int max;
+  int min;
+  int median;
+
   /* Statistics and Printing Functions Go Here */
+
+  print_array(test, SIZE);
+  sort_array(test, SIZE);
+
+  mean = find_mean(test, SIZE);
+  max = find_max(test, SIZE);
+  min = find_min(test, SIZE, max);
+  median = find_median(test, SIZE);
+
+  print_statistics(max, min, median, mean);
 
 }
 
